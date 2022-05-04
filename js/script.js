418a33bcf322e95c2f31efd6b32c5a71b4fe9fd5
unknown <r0785003@student.thomasmore.be>
2022-05-04 18:09:39 +0200

    portpul2

--- js/script.js
+++ js/script.js
@@ -37,11 +37,13 @@ $(document).ready(function(){
 
 });
 
+
+
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("Modal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("Btn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -56,9 +58,76 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+
+
+
+// Get the modal
+var modal2 = document.getElementById("Modal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("Btn2");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
 }
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+
+
+
+
+// Get the modal
+var modal3 = document.getElementById("Modal3");
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("Btn3");
+
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close3")[0];
+
+// When the user clicks on the button, open the modal
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
+
+
+
+
+// Get the modal
+var modal4 = document.getElementById("Modal4");
+
+// Get the button that opens the modal
+var btn4 = document.getElementById("Btn4");
+
+// Get the <span> element that closes the modal
+var span4 = document.getElementsByClassName("close4")[0];
+
+// When the user clicks on the button, open the modal
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+
+
+
+
+
+
