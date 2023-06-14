9ab5f672774c809282fd3d9ded0890fd7d8e3c36
woutdeelkens <r0785003@student.thomasmore.be>
2023-06-14 15:53:27 +0200

    layout and documents added

--- js/script.js
+++ js/script.js
@@ -1,43 +1,34 @@
-$(document).ready(function(){
-
-  $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
   });
 
-  $(window).on('scroll load',function(){
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $("header").removeClass("toggle");
 
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
-
-    if($(window).scrollTop() > 0){
-      $('.top').show();
-    }else{
-      $('.top').hide();
+    if ($(window).scrollTop() > 0) {
+      $(".top").show();
+    } else {
+      $(".top").hide();
     }
-
   });
 
-  // smooth scrolling 
+  // smooth scrolling
 
-  $('a[href*="#"]').on('click',function(e){
-
+  $('a[href*="#"]').on("click", function (e) {
     e.preventDefault();
 
-    $('html, body').animate({
-
-      scrollTop : $($(this).attr('href')).offset().top,
-
-    },
-      500, 
-      'linear'
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
     );
-
   });
-
 });
-
-
 
 // Get the modal
 var modal = document.getElementById("Modal");
@@ -49,17 +40,20 @@ var btn = document.getElementById("Btn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
-
-
-
+};
+//close modal when press ESC
+$(document).keydown(function (event) {
+  if (event.keyCode == 27) {
+    modal.style.display = "none";
+  }
+});
 
 // Get the modal
 var modal2 = document.getElementById("Modal2");
@@ -71,18 +65,20 @@ var btn2 = document.getElementById("Btn2");
 var span2 = document.getElementsByClassName("close2")[0];
 
 // When the user clicks on the button, open the modal
-btn2.onclick = function() {
+btn2.onclick = function () {
   modal2.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span2.onclick = function() {
+span2.onclick = function () {
   modal2.style.display = "none";
-}
-
-
-
-
+};
+//close modal when press ESC
+$(document).keydown(function (event) {
+  if (event.keyCode == 27) {
+    modal2.style.display = "none";
+  }
+});
 
 // Get the modal
 var modal3 = document.getElementById("Modal3");
@@ -94,18 +90,20 @@ var btn3 = document.getElementById("Btn3");
 var span3 = document.getElementsByClassName("close3")[0];
 
 // When the user clicks on the button, open the modal
-btn3.onclick = function() {
+btn3.onclick = function () {
   modal3.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span3.onclick = function() {
+span3.onclick = function () {
   modal3.style.display = "none";
-}
-
-
-
-
+};
+//close modal when press ESC
+$(document).keydown(function (event) {
+  if (event.keyCode == 27) {
+    modal3.style.display = "none";
+  }
+});
 
 // Get the modal
 var modal4 = document.getElementById("Modal4");
@@ -117,17 +115,30 @@ var btn4 = document.getElementById("Btn4");
 var span4 = document.getElementsByClassName("close4")[0];
 
 // When the user clicks on the button, open the modal
-btn4.onclick = function() {
+btn4.onclick = function () {
   modal4.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span4.onclick = function() {
+span4.onclick = function () {
   modal4.style.display = "none";
-}
+};
+//close modal when press ESC
+$(document).keydown(function (event) {
+  if (event.keyCode == 27) {
+    modal4.style.display = "none";
+  }
+});
 
-
-
-
-
-
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  } else if (event.target == modal3) {
+    modal3.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
+  } else if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
